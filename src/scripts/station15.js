771c8242d098480be2218e5726cbfb11383e3daa
syun17 <shunki.s777@gmail.com>
2025-03-03 12:59:10 +0900

    station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,25 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  console.log(userList)
+  console.log(userList.first_name)
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+async function test(data) {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+
+      resolve()
+    }, 3000)
+  })
+  console.log(data)
+  return data.map(buildFullName)
+}
+
+function buildFullName(data) {
+  data.full_name = data.family_name + ' ' + data.first_name;
+  return data;
 }
 
